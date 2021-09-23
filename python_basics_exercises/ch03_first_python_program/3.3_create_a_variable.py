956a4c2da5fdb772f436a02de12d830306c8c1bf
Luis A. Castillo <luisarturocastillo@yahoo.com>
2021-09-23 17:13:00 +1200

    Create 3.3_create_a_variable.py
    These are the solution to the exercises in section 3.3

---
+++ python_basics_exercises/ch03_first_python_program/3.3_create_a_variable.py
@@ -0,0 +1,14 @@
+#3.3 - Create a Variable
+#Review Exercises
+
+#Exercise 1 - Using the interactive window, display some text using print().
+
+print("Hello World!")
+
+#Exercise 2 - Using the interactive window, assign a string literal to a variable. 
+# Then print the contents of the variable using the print() function. 
+
+my_string = "Hello"
+print(my_string)
+
+#Exercise 3 - Repeat the first two exercises using the editor window. (Considered done as per above)
