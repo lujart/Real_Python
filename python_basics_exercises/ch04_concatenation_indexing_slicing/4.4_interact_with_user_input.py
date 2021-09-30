f9d3863655730397203aa51c0033667ab1c7ca73
Luis A. Castillo <luisarturocastillo@yahoo.com>
2021-09-30 16:09:12 +1300

    I made a few adjustments to Exercises 2 and 3.

--- python_basics_exercises/ch04_concatenation_indexing_slicing/4.4_interact_with_user_input.py
+++ python_basics_exercises/ch04_concatenation_indexing_slicing/4.4_interact_with_user_input.py
@@ -8,9 +8,10 @@ print("Your age is " + user_age)
 
 #Exercise 2 - Write a program that takes input from the user and displays the input in lowercase.
 
-user_favourite_colour = input("What is your favorite colour?. Please type the answer in capital letters.")
-print("You answer: " + user_favourite_colour.lower() + "...is now in lower case letters.")
+user_favourite_colour = input("What is your favorite colour?. Please type the answer in capital letters: ")
+print("You answer: " + user_favourite_colour.lower() + " ...is now in lower case letters.")
 
 #Exercise 3 - Write a program from the user and displays the number of characters in the input. 
 
-print("Your previous answer has " + len(userfavourite_colour) + " characters.")
+print("Your previous answer has " + str(len(user_favourite_colour)) + " characters.")
+
