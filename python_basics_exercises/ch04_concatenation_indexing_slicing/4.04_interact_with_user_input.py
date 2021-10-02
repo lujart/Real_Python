c37033c074acdf0538caad366c2a98ef7ab6c82f
Luis A. Castillo <luisarturocastillo@yahoo.com>
2021-10-02 17:55:42 +1300

    Create 4.04_interact_with_user_input.py
    These are my answers for the Review Exercises of Section 4.4 from the Python Basics book.

---
+++ python_basics_exercises/ch04_concatenation_indexing_slicing/4.04_interact_with_user_input.py
@@ -0,0 +1,16 @@
+#Section 4.4 - Interact with User Input
+#Review Exercises
+
+#Exercise 1 - Write a program that takes input from the user and displays that input back.
+
+user_age = input("What is your age?: ")
+print("Your age is " + user_age)
+
+#Exercise 2 - Write a program that takes input from the user and displays the input in lowercase.
+
+user_favourite_colour = input("What is your favorite colour?. Please type the answer in capital letters: ")
+print("You answer: " + user_favourite_colour.lower() + " ...is now in lower case letters.")
+
+#Exercise 3 - Write a program from the user and displays the number of characters in the input. 
+
+print("Your previous answer has " + str(len(user_favourite_colour)) + " characters.")
